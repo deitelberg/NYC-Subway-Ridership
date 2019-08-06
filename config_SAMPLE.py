34a50338a9d3a827fa6d8850b89de37075bff63f
Doodle <deitelberg@gmail.com>
2019-08-06 16:43:38 -0400

    Add get data via API scripts

--- config_SAMPLE.py
+++ config_SAMPLE.py
@@ -1,3 +1,6 @@
+# This is a sample of a config file to protect sensitive information 
+# when using the API to pull NYC subway turnstile data
+
 apiUser = <email address>
 apiPass = <password>
 apiToken = <app token>
